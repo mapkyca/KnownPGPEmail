87e1292f4e147af83f27650a1c5ada46c76ab562
Marcus Povey <marcus@marcus-povey.co.uk>
2015-03-28 11:46:23 +0000

    Initial rough encryption plugin w/ strip tags

---
+++ PGPEmail/Main.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace IdnoPlugins\PGPEmail {
+
+    class Main extends \Idno\Common\Plugin {
+
+	function registerPages() {
+	    
+	}
+
+	function registerEventHooks() {
+	    \Idno\Core\site()->addEventHook('email/send', function(\Idno\Core\Event $event) {
+		$email = $event->data()['email'];
+		$body = $event->response()->getBody();
+		
+		if ($encrypt = $this->encryptto(strip_tags($body), $email->message->getTo())) {
+		    
+		    $email->message->setBody($encrypt, 'text/plain');
+		    
+		    $event->setResponse($email->message);
+		}
+	    });
+	}
+
+	protected function find_encryption_key($keys) {
+
+	    $fingerprint = null;
+	    foreach ($keys as $k) {
+
+		if ((!$k['expired']) && ($k['can_encrypt']) && (!$fingerprint) && (isset($k['fingerprint']))) {
+		    $fingerprint = $k['fingerprint'];
+		}
+
+		if (!$fingerprint && isset($k['subkeys'])) {
+		    $fingerprint = $this->find_encryption_key($k['subkeys']);
+		}
+	    }
+
+	    return $fingerprint;
+	}
+
+	protected function encryptto($message, $address) {
+
+	    $gpg = new \gnupg();
+
+	    if (is_array($address))
+	    {
+		foreach ($address as $k => $v)
+		{
+		    $address = $k;
+		    break;
+		}
+	    }
+	    
+	    // Find keys
+	    $keys = $gpg->keyinfo($address); 
+	    if ($keys) {
+		$fingerprint = $this->find_encryption_key($keys);
+
+		$gpg->addencryptkey($fingerprint);
+
+		return $gpg->encrypt($message);
+	    }
+
+	    return false;
+	}
+
+    }
+
+}
